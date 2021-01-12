36f0c0aa9375fc372ae1a38145e6d1a03c87e043
KaduHod <carlosjr.ribas@gmail.com>
2021-01-11 22:06:59 -0300

    Adicionei funcoes que verificam se array de lesoes e coerentecom o seu musculo e funçoes que filtram os exericios de suas porçoes lesionadas

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-//const importaClasse = require('./_treinos/classeTreino/musculoSimples')//Arquivco contendo a classe
-const importaLesao = require('./_treinos/lesao')// Arquivo contendo a função de lesão
-const impTreino = require('./_treinos/mostraTreino')// Arquivo com a função de mostrar o treino
-require('./_muscles/musculos')
 
-// Chamada ao treino
-
-    /*lesaoDorso = ['Braquial']
-    lesao = []
-    lesaoTrapezio = []
-    //console.log(Peitoral.porcoes)
-
-    var BIcepsLesionado = importaLesao.lesao(Trapezio, lesaoTrapezio)
-    var TreinoTrapezio = impTreino.mostraTreino(BIcepsLesionado)
-
-    var PeitoralVerificaLesão  = importaLesao.lesao(Peitoral, lesao) 
-    var TreinoPeitoral = impTreino.mostraTreino(PeitoralVerificaLesão)
-
-    var DorsoVerificaLesão  = importaLesao.lesao(Dorso, lesaoDorso) 
-    var TreinoDorso = impTreino.mostraTreino(DorsoVerificaLesão)
-    //console.log(BIcepsLesionado)
-    console.log(TreinoTrapezio.exercicios)
-    console.log(TreinoPeitoral.exercicios)
-
-    console.log(TreinoDorso.exercicios)*/
-
-// Criar objetos com exercicios funcionais
 
 
 
