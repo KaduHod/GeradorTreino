9517484a32527084be26740a1be7717ec5994263
KaduHod <carlosjr.ribas@gmail.com>
2021-02-08 23:49:17 -0300

    adição de dados antropometricos ao frontend

--- routes/aluno.js
+++ routes/aluno.js
@@ -14,40 +14,83 @@ router.post('/', (req, res)=>{
 })
 router.post('/verificaCad', (req, res)=>{
 
-    //Pegando o nascimento do formulario
-        data = req.body.nascimento[1]
-        arrayData = data.split('/')
-        
-        dataNascForm = {
-            dia: arrayData[0],
-            mes: arrayData[1],
-            ano: arrayData[2]
+    // verificação de tamanho de string digitada no form
+        arrErrosCadAluno = []
+        for(chaves in req.body){
+            if(req.body[chaves].length < 1){
+                arrErrosCadAluno.push(chaves)
+            }         
         }
 
-    // Pegando dados da classe de 7 dobras cutaneas
-        const SeteDobras = new dadosAntropometricos(parseFloat(req.body.tricipital), parseFloat(req.body.subescapular), parseFloat(req.body.peitoral), parseFloat(req.body.axilar_media), parseFloat(req.body.abdominal), parseFloat(req.body.suprailiaca), parseFloat(req.body.coxa), parseFloat(req.body.dbru),parseFloat(req.body.dbdf ))
-        SeteDobras.Total7dobras()
-    
-    //Criando objeto aluno
-        AlunoNovo = new Aluno(req.body.nome, dataNascForm, req.body.sexo, SeteDobras, parseFloat(req.body.peso), parseFloat(req.body.altura), parseFloat(req.body.circunCintura), parseFloat(req.body.circunQuadril), parseFloat(req.body.circunPunho))
+        if(arrErrosCadAluno.length > 0){
+            res.render('alunos/alunosCad', {error: arrErrosCadAluno})
+        }else{
+             //Pegando o nascimento do formulario
+            data = req.body.nascimento
+            arrayData = data.split('/')
+            
+            dataNascForm = {
+                dia: arrayData[0],
+                mes: arrayData[1],
+                ano: arrayData[2]
+            }
 
-        // acionando metodos!!
-            AlunoNovo.calcDensidade()
-            AlunoNovo.calcGordura()
-            AlunoNovo.calcPesoGordura()
-            AlunoNovo.calcPesoOsseo()
-            AlunoNovo.calcPesoResidual()
-            AlunoNovo.calcPesoMuscular()
-            AlunoNovo.calcMetabolismoBasal()
-            AlunoNovo.calcIMC()
-            AlunoNovo.calcGorduraVisceral()
-            AlunoNovo.calcPorcentagemPeso()
-            AlunoNovo.analisaGordura()
-            AlunoNovo.analisaMusculo()
-            AlunoNovo.calcEstruturaOssea()
-            AlunoNovo.analisaOssos()
+            // Pegando dados da classe de 7 dobras cutaneas
+                const SeteDobras = new dadosAntropometricos(parseFloat(req.body.tricipital), parseFloat(req.body.subescapular), parseFloat(req.body.peitoral), parseFloat(req.body.axilar_media), parseFloat(req.body.abdominal), parseFloat(req.body.suprailiaca), parseFloat(req.body.coxa), parseFloat(req.body.dbru),parseFloat(req.body.dbdf ))
+                SeteDobras.Total7dobras()
+            
+            //Criando objeto aluno
+                AlunoNovo = new Aluno(req.body.nome, dataNascForm, req.body.sexo, SeteDobras, parseFloat(req.body.peso), parseFloat(req.body.altura), parseFloat(req.body.circunCintura), parseFloat(req.body.circunQuadril), parseFloat(req.body.circunPunho))
 
-    console.log(AlunoNovo)
+                // acionando metodos!!
+                    AlunoNovo.calcDensidade()
+                    AlunoNovo.calcGordura()
+                    AlunoNovo.calcPesoGordura()
+                    AlunoNovo.calcPesoOsseo()
+                    AlunoNovo.calcPesoResidual()
+                    AlunoNovo.calcPesoMuscular()
+                    AlunoNovo.calcMetabolismoBasal()
+                    AlunoNovo.calcIMC()
+                    AlunoNovo.calcGorduraVisceral()
+                    AlunoNovo.calcPorcentagemPeso()
+                    AlunoNovo.analisaGordura()
+                    AlunoNovo.analisaMusculo()
+                    AlunoNovo.calcEstruturaOssea()
+                    AlunoNovo.analisaOssos()
+
+                    // Dados gerais
+                    geral = AlunoNovo.objNSNIPA
+                    geral.nascimento = data
+
+                    //Dados de peso
+                    pesos = AlunoNovo.objPesoKG_RMOG
+
+                    //Dados de porcentagem
+                    porcem = AlunoNovo.objPorcem
+                    
+                    //Informações adicionais
+                    InfoAdcional = {
+                        dc: AlunoNovo.Densidade_Corporal,
+                        imc: AlunoNovo.Imc,
+                        Mb: AlunoNovo.Metabolismo_Basal
+                    }
+
+                    //Taixas
+                    taixas = {
+                        mr: AlunoNovo.muscle_Rate,
+                        br: AlunoNovo.bone_Rate,
+                        bf: AlunoNovo.Body_Fat_rate
+                    }
+                    
+
+
+
+            res.render('alunos/alunosInicio', {dadoGerais: geral, pesos : pesos, porcem: porcem, infoAdi: InfoAdcional, taixas: taixas })
+        }
+
+   
+
+
 
     
 })
